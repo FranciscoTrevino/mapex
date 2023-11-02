54d18b783af140933c0ee06ab68892f5f37ba983
FranciscoTrevino <88327545+FranciscoTrevino@users.noreply.github.com>
2023-11-01 21:53:54 -0600

    Agregada la función de punto A a punto B (simple)

--- lib/main.dart
+++ lib/main.dart
@@ -1,170 +1,3 @@
-/*
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: kIsWeb ? WebMapPage() : AndroidiOSMapPage(),
-    );
-  }
-}
-
-class WebMapPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Mapa en la Web"),
-      ),
-      // Agregar aquí el mapa para la web.
-    );
-  }
-}
-
-class AndroidiOSMapPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Mapa en Android/iOS"),
-      ),
-      // Agregar aquí el mapa para Android e iOS.
-    );
-  }
-}*/
-
-/*
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MapWebView(),
-    );
-  }
-}
-
-class MapWebView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mapa de Ejemplo'),
-      ),
-      body: const WebView(
-        initialUrl: 'https://www.google.com/maps', // URL de Google Maps u otro servicio de mapas
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
-    );
-  }
-}*/
-
-/*
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MapsPage(),
-    );
-  }
-}
-
-class MapsPage extends StatelessWidget {
-  static String id = 'maps_page';
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      drawer: DrawerCashVolt(),
-      body: Stack(
-        children: [
-          Positioned(
-            top: 50,
-            left: 10,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: IconButtonDrawer(
-                onPressed: () {
-                  final scaffoldState = _scaffoldKey.currentState;
-                  if (scaffoldState != null) {
-                    scaffoldState.openDrawer();
-                  }
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class DrawerCashVolt extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Define el contenido de tu cajón (drawer) aquí
-    return Drawer(
-      child: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('Opción 1'),
-            onTap: () {
-              // Acción al hacer clic en la opción 1
-            },
-          ),
-          ListTile(
-            title: Text('Opción 2'),
-            onTap: () {
-              // Acción al hacer clic en la opción 2
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class IconButtonDrawer extends StatelessWidget {
-  final VoidCallback onPressed;
-
-  IconButtonDrawer({required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: onPressed,
-      icon: Icon(Icons.menu),
-    );
-  }
-}
-*/
-
-
-
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -181,22 +14,146 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class RouteMap extends StatelessWidget {
+class RouteMap extends StatefulWidget {
+  @override
+  _RouteMapState createState() => _RouteMapState();
+}
+
+class _RouteMapState extends State<RouteMap> {
+  final List<String> options1 = ['Opción 1', 'Opción 2'];
+  final List<String> options2 = ['Opción A', 'Opción B'];
+  String selectedOption1 = 'Opción 1'; // Opción predeterminada
+  String selectedOption2 = 'Opción A'; // Opción predeterminada
+  late List<Offset> controlPoints;
+  late Offset startPoint; // Punto de inicio
+
+  @override
+  void initState() {
+    super.initState();
+    updateControlPoints();
+  }
+
+  void updateControlPoints() {
+    setState(() {
+      if (selectedOption1 == 'Opción 1' && selectedOption2 == 'Opción A') {
+        controlPoints = [
+          // Puntos para la combinación Opción 1 y Opción A
+          const Offset(200, 170),
+          const Offset(200, 165),
+          const Offset(220, 180),
+          const Offset(220, 185),
+          const Offset(230, 170),
+          const Offset(232, 160),
+          const Offset(240, 160),
+          const Offset(250, 160),
+          const Offset(260, 162),
+          const Offset(270, 162),
+          const Offset(280, 164),
+          const Offset(284, 164.5),
+          const Offset(287, 162),
+          const Offset(288, 162),
+          const Offset(289, 161),
+          const Offset(290, 161),
+          const Offset(292, 157),
+          const Offset(294, 154),
+          const Offset(294, 151),
+          const Offset(294, 147),
+          const Offset(294, 130),
+
+          // Añade más puntos aquí según tu necesidad
+        ];
+        startPoint = const Offset(190, 180); // Punto de inicio para esta combinación
+      } else if (selectedOption1 == 'Opción 1' && selectedOption2 == 'Opción B') {
+        controlPoints = [
+          // Puntos para la combinación Opción 1 y Opción B
+          const Offset(260, 127),  // Ajusta la ubicación del punto de inicio
+          const Offset(260, 180),
+          const Offset(270, 190),
+          // Añade más puntos aquí según tu necesidad
+        ];
+        startPoint = const Offset(294, 130); // Punto de inicio para esta combinación
+      } else if (selectedOption1 == 'Opción 2' && selectedOption2 == 'Opción A') {
+        controlPoints = [
+          // Puntos para la combinación Opción 2 y Opción A
+          const Offset(250, 170),  // Ajusta la ubicación del punto de inicio
+          const Offset(260, 180),
+          const Offset(270, 190),
+        ];
+        startPoint = const Offset(150, 160); // Punto de inicio para esta combinación
+      } else if (selectedOption1 == 'Opción 2' && selectedOption2 == 'Opción B') {
+        controlPoints = [
+          // Puntos para la combinación Opción 2 y Opción B
+          const Offset(200, 170),
+          const Offset(200, 165),
+          const Offset(220, 180),
+        ];
+        startPoint = const Offset(180, 170); // Punto de inicio para esta combinación
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Trazar Rutas en una Imagen'),
       ),
-      body: Stack(
-        children: <Widget>[
-          // Imagen de fondo (puedes usar AssetImage o NetworkImage)
-          Image.asset('assets/mapa.jpg'),
-
-          // Ejemplo de trazar una ruta desde el punto (100, 150)
-          CustomPaint(
-            painter: RoutePainter(),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DropdownButton<String>(
+                value: selectedOption1,
+                items: options1.map((String option) {
+                  return DropdownMenuItem<String>(
+                    value: option,
+                    child: Text(option),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      selectedOption1 = newValue;
+                      updateControlPoints();
+                    });
+                  }
+                },
+              ),
+              SizedBox(width: 16.0),
+              DropdownButton<String>(
+                value: selectedOption2,
+                items: options2.map((String option) {
+                  return DropdownMenuItem<String>(
+                    value: option,
+                    child: Text(option),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  if (newValue != null) {
+                    setState(() {
+                      selectedOption2 = newValue;
+                      updateControlPoints();
+                    });
+                  }
+                },
+              ),
+            ],
           ),
+          Expanded(
+            child: Stack(
+              children: <Widget>[
+                // Imagen de fondo (puedes usar AssetImage o NetworkImage)
+                Image.asset('assets/mapa.jpg'),
+
+                // Ejemplo de trazar una ruta desde el punto de inicio
+                CustomPaint(
+                  painter: RoutePainter(controlPoints, startPoint),
+                ),
+              ],
+            ),
+          ),
+          Text("Las opciones con numeros son el inicio y las opciones con letra son el final"),
         ],
       ),
     );
@@ -204,42 +161,21 @@ class RouteMap extends StatelessWidget {
 }
 
 class RoutePainter extends CustomPainter {
+  final List<Offset> controlPoints;
+  final Offset startPoint;
+
+  RoutePainter(this.controlPoints, this.startPoint);
+
   @override
   void paint(Canvas canvas, Size size) {
     final Paint paint = Paint()
       ..color = Colors.red
       ..strokeCap = StrokeCap.round
-      ..style = PaintingStyle.stroke  // Establece el estilo de dibujo en línea
+      ..style = PaintingStyle.stroke
       ..strokeWidth = 3.0;
 
     final path = Path();
-    path.moveTo(190, 180);  // Punto de inicio
-
-    // En esta parte es donde cambias los puntos por donde pasa la línea
-    final List<Offset> controlPoints = [
-      const Offset(200, 170),
-      const Offset(200, 165),
-      const Offset(220, 180),
-      const Offset(220, 185),
-      const Offset(230, 170),
-      const Offset(232, 160),
-      const Offset(240, 160),
-      const Offset(250, 160),
-      const Offset(260, 162),
-      const Offset(270, 162),
-      const Offset(280, 164),
-      const Offset(284, 164.5),
-      const Offset(287, 162),
-      const Offset(288, 162),
-      const Offset(289, 161),
-      const Offset(290, 161),
-      const Offset(292, 157),
-      const Offset(294, 154),
-      const Offset(294, 151),
-      const Offset(294, 147),
-      const Offset(294, 137),
-
-    ];
+    path.moveTo(startPoint.dx, startPoint.dy);  // Punto de inicio
 
     // Genera la línea curva
     for (double t = 0.0; t <= 1.0; t += 0.01) {
@@ -251,7 +187,6 @@ class RoutePainter extends CustomPainter {
   }
 
   Offset _getPointOnCurve(List<Offset> controlPoints, double t) {
-    // Calcula el punto en la curva de Bezier utilizando los puntos de control
     final x = _bezierPoint(t, controlPoints.map((p) => p.dx).toList());
     final y = _bezierPoint(t, controlPoints.map((p) => p.dy).toList());
     return Offset(x, y);
